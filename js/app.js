65e1da41021d1770e2240af76e1ec982da282160
Darius Kazemi <darius.kazemi@gmail.com>
2014-04-26 13:07:46 -0400

    adding more samples

--- js/app.js
+++ js/app.js
@@ -3,4 +3,6 @@ $(document).ready( function() {
     'urlPrefix':'audio/'
   });
   lowLag.load(['baby.wav'],'baby');
+  lowLag.load(['flourish.wav'],'flourish');
+  lowLag.load(['snarechord1.wav'],'snarechord');
 });
